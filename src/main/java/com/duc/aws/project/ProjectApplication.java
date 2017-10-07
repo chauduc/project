5eb2f279f4ee717564275f6233ca4f9d17075de2
Duc|GC-ドゥク <d-chau@gcrest.com>
2017-10-08 01:21:37 +0900

    no message

--- src/main/java/com/duc/aws/project/ProjectApplication.java
+++ src/main/java/com/duc/aws/project/ProjectApplication.java
@@ -1,12 +1,15 @@
 package com.duc.aws.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ServletComponentScan(value="com.duc.aws.project.security")
 @ComponentScan("com.duc.aws.project") 
 public class ProjectApplication {
